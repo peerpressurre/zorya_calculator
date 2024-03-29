00692617562b583921c0497b1c9c57bde6a534c5
peerpressurre <gsophiay@gmail.com>
2024-03-30 01:47:04 +0200

    b

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -1,6 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -8,39 +9,59 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: 
-      Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage("assets/background.jpg"), fit: BoxFit.fill)
-            ),
-          ),
-          Center(
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.7,
-              width: MediaQuery.of(context).size.width * 0.7,
+      body: Center(
+        child: Stack(
+          children: [
+            Container(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(13),
-                color: Color(0xFFE0B76F),
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Text('Розтермінування від забудовника',
-                      style: GoogleFonts.manrope(color: Colors.black,
-                     fontSize: 25), 
-                      )
-                    ],
-                  )
-                ],
+                image: DecorationImage(
+                  image: AssetImage("assets/background.jpg"),
+                  fit: BoxFit.fill,
+                ),
               ),
             ),
-          )
-        ],
-      )
-      )
+            // Container(
+            //   color: Colors.black.withOpacity(0.2),
+            // ),
+            Center(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(13),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 7.0, sigmaY: 7.0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.8,
+                    width: MediaQuery.of(context).size.width * 0.85,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(13),
+                      // color: const Color(0xFF4A4647).withOpacity(0.8),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 15, top: 10),
+                          child: Row(
+                            children: [
+                              Text('Розтермінування від забудовника',
+                                style: GoogleFonts.manrope(
+                                  color: Colors.black,
+                                )
+                                // TextStyle(
+                                //   color: Colors.black,
+                                //   fontSize: 25,
+                                // ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
