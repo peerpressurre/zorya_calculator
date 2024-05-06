bdc7ef651482f62ef8da0e1bd558d508f0346714
peerpressurre <gsophiay@gmail.com>
2024-05-06 23:50:02 +0300

    74967

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -20,6 +19,14 @@ class Home extends StatelessWidget {
                 ),
               ),
             ),
+             Positioned.fill(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5), // Adjust sigmaX and sigmaY for blur intensity
+            child: Container(
+              color: Colors.transparent,
+            ),
+          ),
+        ),
             // Container(
             //   color: Colors.black.withOpacity(0.2),
             // ),
@@ -27,13 +34,16 @@ class Home extends StatelessWidget {
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(13),
                 child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 7.0, sigmaY: 7.0),
+                   filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
                   child: Container(
+                    padding: EdgeInsets.all(40.0),
                     height: MediaQuery.of(context).size.height * 0.8,
                     width: MediaQuery.of(context).size.width * 0.85,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(13),
                       // color: const Color(0xFF4A4647).withOpacity(0.8),
+                      color: Colors.white.withOpacity(0.7)
+                      
                     ),
                     child: Column(
                       children: [
@@ -41,15 +51,16 @@ class Home extends StatelessWidget {
                           padding: EdgeInsets.only(left: 15, top: 10),
                           child: Row(
                             children: [
-                              Text('Розтермінування від забудовника',
-                                style: GoogleFonts.manrope(
-                                  color: Colors.black,
-                                )
-                                // TextStyle(
-                                //   color: Colors.black,
-                                //   fontSize: 25,
-                                // ),
-                              )
+                              // Text('Розтермінування від забудовника',
+                              //   style:
+                              //   //  GoogleFonts.manrope(
+                              //   //   color: Colors.black,
+                              //   // )
+                              //   TextStyle(
+                              //     color: Colors.black,
+                              //     fontSize: 25,
+                              //   ),
+                              // )
                             ],
                           ),
                         )
